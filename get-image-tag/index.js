40eab68c58e78de08cc860442ac706f46f07560a
lnetrebskii <47530506+lnetrebskii@users.noreply.github.com>
2020-06-09 17:45:04 +0400

    VP-2753: Hardcode version number temporary

--- get-image-tag/index.js
+++ get-image-tag/index.js
@@ -8,23 +8,17 @@ String.isNullOrEmpty = function(value) {
     return !(typeof value === "string" && value.length > 0);
 }
 
-fs.readFile('Directory.Build.Props', function (err, data) {
-    if (!err) {
-        parser.parseString(data, function (err, json) {
-            if (!err) {
-                var prefix = json.Project.PropertyGroup[1].VersionPrefix[0].trim();
-                var suffix = json.Project.PropertyGroup[1].VersionSuffix[0].trim();
 
-                sha = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha;
-        
-                let version = prefix + (suffix != '' ? '-' + suffix : '') + '-' + sha.substring(0, 8);
-        
-                core.setOutput("tag", version);
-                core.setOutput("sha", sha);
-        
-                console.log(`Version tag is: ${version}`);
-                console.log(`Head sha is: ${sha}`);
-            }
-        });
-    }
-});
+
+var prefix = '5.0.0';
+var suffix = '';
+
+sha = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha;
+
+let version = prefix + (suffix != '' ? '-' + suffix : '') + '-' + sha.substring(0, 8);
+
+core.setOutput("tag", version);
+core.setOutput("sha", sha);
+
+console.log(`Version tag is: ${version}`);
+console.log(`Head sha is: ${sha}`);
