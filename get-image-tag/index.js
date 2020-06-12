6fd8ea4baff8a1d99c6b12bd7ca96c61057c21b6
lnetrebskii <47530506+lnetrebskii@users.noreply.github.com>
2020-06-12 10:24:59 +0400

    Update index.js

--- get-image-tag/index.js
+++ get-image-tag/index.js
@@ -8,17 +8,31 @@ String.isNullOrEmpty = function(value) {
     return !(typeof value === "string" && value.length > 0);
 }
 
+let path = 'Directory.Build.Props';
+if (!fs.existsSync(path)) {
+    path = 'Directory.Build.props';
+}
 
+fs.readFile(path, function (err, data) {
+    if (!err) {
+        parser.parseString(data, function (err, json) {
+            if (!err) {
+                var prefix = json.Project.PropertyGroup[1].VersionPrefix[0].trim();
+                var suffix = json.Project.PropertyGroup[1].VersionSuffix[0].trim();
 
-var prefix = '5.0.0';
-var suffix = '';
-
-sha = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha;
-
-let version = prefix + (suffix != '' ? '-' + suffix : '') + '-' + sha.substring(0, 8);
-
-core.setOutput("tag", version);
-core.setOutput("sha", sha);
-
-console.log(`Version tag is: ${version}`);
-console.log(`Head sha is: ${sha}`);
+                sha = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha;
+        
+                let version = prefix + (suffix != '' ? '-' + suffix : '') + '-' + sha.substring(0, 8);
+        
+                core.setOutput("tag", version);
+                core.setOutput("sha", sha);
+        
+                console.log(`Version tag is: ${version}`);
+                console.log(`Head sha is: ${sha}`);
+            }
+        });
+    }
+    else {
+        console.log(`Cannot load file ${path}`);
+    }
+});
