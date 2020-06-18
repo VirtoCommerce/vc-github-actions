32806fcee932b1f64640b8197144f7489c414eea
Leo <leon@entechsolutions.com>
2020-06-18 14:40:06 +0400

    VP-3187: Fix push

--- sync-module-cicd/entrypoint.ps1
+++ sync-module-cicd/entrypoint.ps1
@@ -1,4 +1,5 @@
 param (
+    [string]$GitHubToken,
     [string]$TargetRepository = $env:GITHUB_REPOSITORY,
     [string]$SourceRepository = "vc-module-order",
     [string]$BranchToSync = "feature/vp-3187-initial-ci"
@@ -14,13 +15,15 @@ git clone "https://github.com/$($TargetRepository)"
 cd $repoDir
 git checkout $BranchToSync
 
-git config --global user.email "noreply@virtoway.com"
+git config --global user.email "github.actions@virtoway.com"
 git config --global user.name "GitHub Actions"
+
+$repo = "https://github-actions:$($GitHubToken)@github.com/$($TargetRepository).git"
 
 Get-ChildItem -Path "../$($SourceRepository)/.github/workflows" -Filter "*.yml" | ForEach-Object {
     $file = ".github/workflows/$($_.name)"
     Copy-Item $_ $file
     git add $file
     git commit -m "Sync $($file) from $($SourceRepository)"
-    git push
+    git push $repo
 }
