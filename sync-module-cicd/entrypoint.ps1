e515c38bc8e40908185e96b4440a3a4184242898
Leo <leon@entechsolutions.com>
2020-06-18 13:28:57 +0400

    VP-3187: Push changes to the target repository

--- sync-module-cicd/entrypoint.ps1
+++ sync-module-cicd/entrypoint.ps1
@@ -16,5 +16,9 @@ git checkout $BranchToSync
 cd ..
 
 Get-ChildItem -Path "$($SourceRepository)/.github/workflows" -Filter "*.yml" | ForEach-Object {
-    Copy-Item $_ "$($repoDir)/.github/workflows/$($_.name)"
+    $file = "$($repoDir)/.github/workflows/$($_.name)"
+    Copy-Item $_ $file
+    git add $file
+    git commit "Sync $($file) from $($SourceRepository)"
+    git push
 }
