3a3dafe21c8e60d3ba723ee37aac0b04eab98619
Leo <leon@entechsolutions.com>
2020-06-18 13:36:27 +0400

    VP-3187: Fix path to file

--- sync-module-cicd/entrypoint.ps1
+++ sync-module-cicd/entrypoint.ps1
@@ -15,7 +15,7 @@ cd $repoDir
 git checkout $BranchToSync
 
 Get-ChildItem -Path "../$($SourceRepository)/.github/workflows" -Filter "*.yml" | ForEach-Object {
-    $file = "$($repoDir)/.github/workflows/$($_.name)"
+    $file = ".github/workflows/$($_.name)"
     Copy-Item $_ $file
     git add $file
     git commit "Sync $($file) from $($SourceRepository)"
