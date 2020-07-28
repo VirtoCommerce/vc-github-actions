5ea117aef36fa62219a3ce8389e9e79e59e4a506
Alexander Morogov <asmorogov@gmail.com>
2020-07-28 10:36:30 +0200

    added sonar scanner action

---
+++ sonar-scanner-begin/index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+let isPullRequest = github.context.eventName === 'pull_request';
+let prArg = isPullRequest ? '-PullRequest' : '';
+let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
+if (branchName.indexOf('refs/heads/') > -1) {
+    branchName = branchName.slice('refs/heads/'.length);
+}
+let repoName = process.env.GITHUB_REPOSITORY.slice('VirtoCommerce/'.length);
+if(isPullRequest)
+{
+    process.env.CHANGE_TARGET = github.event.pull_request.base.ref;
+    process.env.CHANGE_TITLE = github.event.pull_request.title;
+    process.env.CHANGE_ID = github.event.number;
+}
+let SonarAuthToken = process.env.SONAR_TOKEN;
+let sonarAuthArg = `-SonarAuthToken ${SonarAuthToken}`;
+let repoNameArg = `-RepoName ${repoName}`;
+
+await exec.exec(`vc-build SonarQubeStart ${prArg} ${sonarAuthArg} ${repoNameArg}`);
