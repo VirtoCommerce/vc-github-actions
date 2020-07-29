7c6ebd7bb01be171567688fb82078f9e04ace1a4
Alexander Morogov <asmorogov@gmail.com>
2020-07-29 14:57:26 +0200

    fix

--- get-image-version/index.js
+++ get-image-version/index.js
@@ -12,7 +12,7 @@ const parser = new xml2js.Parser();
 const src = __dirname;
 
 function findFile(base, name, files, result) {
-    var result = {};
+    var result = [];
     if (fs.existsSync(base)) {
         files = files || fs.readdirSync(base)
         result = result || []
