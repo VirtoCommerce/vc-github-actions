1e0eb9b91f169673e7206f7c6fad7f17bb125f7e
Alexander Morogov <asmorogov@gmail.com>
2020-07-30 09:48:43 +0200

    added changelog generator

---
+++ changelog-generator/index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exception } = require('console');
+
+let orgName = process.env.GITHUB_REPOSITORY.split('/')[0];
+let repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
+
+async function getLatestRelease(orgName, repoName)
+{
+    let releases = JSON.parse(github.repos.listReleases({
+        orgName,
+        repoName
+    }));
+    for(let release of releases)
+    {
+        if(!release.name.startsWith("v2") && release.prerelease === false)
+        {
+            return release;
+        }
+    }
+    throw new exception("No github releases found");
+}
+
+async function getCommitMessages(since)
+{
+    let output = '';
+    let err = '';
+
+    // These are option configurations for the @actions/exec lib`
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        },
+        stderr: (data) => {
+            err += data.toString();
+        }
+    };
+    //options.cwd = './';
+
+    //await exec.exec(`${src}/commit-count.sh`, [baseBranch], options);
+    await exec.exec(`git log --pretty=format:"%s (%h)" --since="${since}"`, [], options).then(exitCode => console.log(`git log --pretty=format:"%s (%h)" --since exitCode: ${exitCode}`));
+    const commitMessages = output;
+    return commitMessages;
+}
+
+let latestRelease = await getLatestRelease(orgName, repoName);
+let commitMessages = await getCommitMessages(latestRelease.published_at);
+core.setOutput("changelog", commitMessages);
