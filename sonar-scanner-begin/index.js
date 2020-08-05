41655908eb8e7fc69e21afe2c06a03e288e6f9a2
Alexander Morogov <asmorogov@gmail.com>
2020-08-05 12:54:03 +0200

    Remove debug messages

--- sonar-scanner-begin/index.js
+++ sonar-scanner-begin/index.js
@@ -3,9 +3,8 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 let isPullRequest = github.context.eventName === 'pull_request';
+
 let prArg = isPullRequest ? '-PullRequest' : '';
-console.log(`github.context.eventName ${github.context.eventName} === pull _request ? ${github.context.eventName === 'pull_request'}`);
-console.log(`${github.context.payload.pull_request} : ${github.context.ref}`);
 let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.base.ref : github.context.ref;
 if (branchName.indexOf('refs/heads/') > -1) {
     branchName = branchName.slice('refs/heads/'.length);
@@ -20,7 +19,5 @@ if(isPullRequest)
 let SonarAuthToken = process.env.SONAR_TOKEN;
 let sonarAuthArg = `-SonarAuthToken ${SonarAuthToken}`;
 let repoNameArg = `-RepoName ${repoName}`;
-
-console.log(`Debug: ${prArg} ${sonarAuthArg} ${repoNameArg} ${process.env.GITHUB_REPOSITORY} ${branchName} ${process.env.SONAR_TOKEN.length}`)
 
 exec.exec(`vc-build SonarQubeStart ${prArg} ${sonarAuthArg} ${repoNameArg}`);
