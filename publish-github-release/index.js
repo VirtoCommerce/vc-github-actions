3f367b3a5ea7cdf06dc92fd83a4d962e879ebe28
Alexander Morogov <asmorogov@gmail.com>
2020-08-05 14:11:12 +0200

    Fix for publish-github-release

--- publish-github-release/index.js
+++ publish-github-release/index.js
@@ -3,55 +3,63 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 const glob = require('glob').Glob;
 const path = require('path');
-const { exit } = require('process');
 
-let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
-let customModuleDownloadUrl = ""
-if (branchName.indexOf('refs/heads/') > -1) {
-    branchName = branchName.slice('refs/heads/'.length);
-}
-if(branchName === 'dev')
+async function run()
 {
-    let blobUrl = `https://vc3prerelease.blob.core.windows.net/packages${process.env.BLOB_SAS}`;
-    let artifactPath = "";
-    glob("artifacts/*.zip", (err, matches) => {
-        if(err != null || matches.length < 1)
-        {
-            core.setFailed(err);
-        }
-        artifactPath = matches[0];
-    });
-    let artifactFileName = artifactPath.split(path.sep).pop();
-    let downloadUrl = `https://vc3prerelease.blob.core.windows.net/packages/${artifactFileName}`;
-    exec.exec(`azcopy10 copy ${artifactPath} ${blobUrl}`).then( exitCode => {
-        if(exitCode != 0)
-        {
-            core.setFailed("azcopy failed");
-        }
-    });
-    customModuleDownloadUrl = `-CustomModulePackageUri ${downloadUrl}`;
-} 
-else if(branchName === 'master')
-{
-    let orgName = process.env.GITHUB_REPOSITORY.split('/')[0];
-    let changelog = core.getInput('changelog');
-    let releaseNotesArg = `-ReleaseNotes ${changelog}`;
-    exec.exec(`vc-build Release -GitHubUser ${orgName} -GitHubToken ${process.env.GITHUB_TOKEN} -ReleaseBranch ${branchName} ${releaseNotesArg} -skip Clean+Restore+Compile+Test`).then(exitCode => {
-        if(exitCode != 0 || exitCode != 422)
-        {
-            core.setFailed("Failed to release");
-        }
-    });
+    let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
+    let customModuleDownloadUrl = ""
+    if (branchName.indexOf('refs/heads/') > -1) {
+        branchName = branchName.slice('refs/heads/'.length);
+    }
+    if(branchName === 'dev')
+    {
+        let blobUrl = `https://vc3prerelease.blob.core.windows.net/packages${process.env.BLOB_SAS}`;
+        let artifactPath = "";
+        await glob("artifacts/*.zip", (err, matches) => {
+            if(err != null || matches.length < 1)
+            {
+                core.setFailed(err);
+                process.exit(1);
+            }
+            artifactPath = matches[0];
+        });
+        let artifactFileName = artifactPath.split(path.sep).pop();
+        let downloadUrl = `https://vc3prerelease.blob.core.windows.net/packages/${artifactFileName}`;
+        await exec.exec(`azcopy10 copy ${artifactPath} ${blobUrl}`).then( exitCode => {
+            if(exitCode != 0)
+            {
+                core.setFailed("azcopy failed");
+                process.exit(exitCode);
+            }
+        });
+        customModuleDownloadUrl = `-CustomModulePackageUri ${downloadUrl}`;
+    } 
+    else if(branchName === 'master')
+    {
+        let orgName = process.env.GITHUB_REPOSITORY.split('/')[0];
+        let changelog = core.getInput('changelog');
+        let releaseNotesArg = `-ReleaseNotes ${changelog}`;
+        await exec.exec(`vc-build Release -GitHubUser ${orgName} -GitHubToken ${process.env.GITHUB_TOKEN} -ReleaseBranch ${branchName} ${releaseNotesArg} -skip Clean+Restore+Compile+Test`).then(exitCode => {
+            if(exitCode != 0 || exitCode != 422)
+            {
+                core.setFailed("Failed to release");
+                process.exit(exitCode);
+            }
+        });
+    }
+
+    if(branchName === 'dev' || branchName === 'master')
+    {
+        await exec.exec(`git config --global user.email "ci@virtocommerce.com"`);
+        await exec.exec(`git config --global user.name "vc-ci"`);
+        await exec.exec(`vc-build PublishModuleManifest ${customModuleDownloadUrl}`).then(exitCode => {
+            if(exitCode != 0 || exitCode != 423)
+            {
+                core.setFailed("Failed to update modules.json");
+                process.exit(exitCode);
+            }
+        });
+    }
 }
 
-if(branchName === 'dev' || branchName === 'master')
-{
-    exec.exec(`git config --global user.email "ci@virtocommerce.com"`);
-    exec.exec(`git config --global user.name "vc-ci"`);
-    exec.exec(`vc-build PublishModuleManifest ${customModuleDownloadUrl}`).then(exitCode => {
-        if(exitCode != 0 || exitCode != 423)
-        {
-            core.setFailed("Failed to update modules.json");
-        }
-    });
-}
+run();
