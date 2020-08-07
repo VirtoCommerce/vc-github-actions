5da8edf767e3d4f3ada88936b1e61e1f0a62adee
Alexander Morogov <asmorogov@gmail.com>
2020-08-07 10:57:48 +0200

    Add catch block

--- publish-github-release/index.js
+++ publish-github-release/index.js
@@ -77,6 +77,8 @@ async function run()
 
     if(branchName === 'dev' || branchName === 'master')
     {
+        await exec.exec(`git config --global user.email "ci@virtocommerce.com"`);
+        await exec.exec(`git config --global user.name "vc-ci"`);
         await setupCredentials('vc-ci', process.env.GITHUB_TOKEN);
         await exec.exec(`vc-build PublishModuleManifest ${customModuleDownloadUrl}`).then(exitCode => {
             if(exitCode != 0 || exitCode != 423)
@@ -88,4 +90,6 @@ async function run()
     }
 }
 
-run();
+run().catch(error => {
+	core.setFailed(error.message);
+});
