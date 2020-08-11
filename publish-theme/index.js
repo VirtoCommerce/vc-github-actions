ebaa0cf26ceee301c0598a6d04ffa08ee3d5711e
Alexander Morogov <asmorogov@gmail.com>
2020-08-11 15:17:42 +0200

    Fix missed dependency

--- publish-theme/index.js
+++ publish-theme/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
+const glob = require('glob');
 
 async function findArtifact(pattern)
 {
