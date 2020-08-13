8addaa3096c608e3cf59a7b198deac4071e42d4f
Alexander Morogov <asmorogov@gmail.com>
2020-08-13 12:26:28 +0200

    fix

--- publish-theme/index.js
+++ publish-theme/index.js
@@ -4,6 +4,11 @@ const exec = require('@actions/exec');
 const path = require('path');
 const glob = require('glob');
 
+let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
+if (branchName.indexOf('refs/heads/') > -1) {
+    branchName = branchName.slice('refs/heads/'.length);
+}
+
 async function findArtifact(pattern)
 {
     let globResult = glob.sync(pattern);
@@ -13,14 +18,13 @@ async function findArtifact(pattern)
 
 async function run()
 {
-    let branchName = github.context.eventName === 'pull_request' ? github.context.payload.pull_request.head.ref : github.context.ref;
-    if (branchName.indexOf('refs/heads/') > -1) {
-        branchName = branchName.slice('refs/heads/'.length);
-    }
+    
     let artifactPath = await findArtifact("artifacts/*.zip");
     console.log(artifactPath);
     let artifactFileName = artifactPath.split(path.sep).pop();
     console.log(artifactFileName);
+    let blobUrl = `https://vc3prerelease.blob.core.windows.net/packages/${artifactFileName}`;
+    console.log(`Blob url: ${blobUrl}`);
     core.setOutput('artifactPath', artifactPath);
     core.setOutput('artifactName', artifactFileName);
     if(branchName === 'dev')
@@ -39,4 +43,7 @@ async function run()
     }
 }
 
-run().catch(err => core.setFailed(err.message));
+if(branchName === 'dev')
+{
+    run().catch(err => core.setFailed(err.message));
+}
