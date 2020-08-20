a2c94e115bfb94b7c61b3ac7fee440c3311e51c5
Alexander Morogov <asmorogov@gmail.com>
2020-08-20 15:21:15 +0200

    refactoring

--- sonar-scanner-begin/index.js
+++ sonar-scanner-begin/index.js
@@ -3,23 +3,23 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 const utils = require('@krankenbro/virto-actions-lib');
 
-let isPullRequest = await utils.isPullRequest(github);
-
-let prArg = isPullRequest ? '-PullRequest' : '';
-let branchName = await utils.getBranchName(github);
-let repoName = await utils.getRepoName();
-if(isPullRequest)
-{
-    process.env.CHANGE_TARGET = github.context.payload.pull_request.base.ref;
-    process.env.CHANGE_TITLE = github.context.payload.pull_request.title;
-    process.env.CHANGE_ID = github.context.payload.pull_request.number;
-}
-let SonarAuthToken = process.env.SONAR_TOKEN;
-let sonarAuthArg = `-SonarAuthToken ${SonarAuthToken}`;
-let repoNameArg = `-RepoName ${repoName}`;
-
 async function run()
 {
+    let isPullRequest = await utils.isPullRequest(github);
+
+    let prArg = isPullRequest ? '-PullRequest' : '';
+    let branchName = await utils.getBranchName(github);
+    let repoName = await utils.getRepoName();
+    if(isPullRequest)
+    {
+        process.env.CHANGE_TARGET = github.context.payload.pull_request.base.ref;
+        process.env.CHANGE_TITLE = github.context.payload.pull_request.title;
+        process.env.CHANGE_ID = github.context.payload.pull_request.number;
+    }
+    let SonarAuthToken = process.env.SONAR_TOKEN;
+    let sonarAuthArg = `-SonarAuthToken ${SonarAuthToken}`;
+    let repoNameArg = `-RepoName ${repoName}`;
+
     await exec.exec(`vc-build SonarQubeStart ${prArg} ${sonarAuthArg} ${repoNameArg}`);
 }
 
