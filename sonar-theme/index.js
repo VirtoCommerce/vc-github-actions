80a8fb9696d83d16ab12b242f5304818b41d19aa
Alexandr Morogov <42555001+krankenbro@users.noreply.github.com>
2020-09-15 15:21:15 +0200

    VP-4625: Refactoring (#19)

--- sonar-theme/index.js
+++ sonar-theme/index.js
@@ -4,14 +4,13 @@ const exec = require('@actions/exec');
 const utils = require('@krankenbro/virto-actions-lib');
 const { runInContext } = require('vm');
 
-let isPullRequest = await utils.isPullRequest(github);
-
-let branchName = await utils.getBranchName(github);
-let repoName = await utils.getRepoName();
-let projectKey = process.env.GITHUB_REPOSITORY.replace('/', '_');
-
 async function run()
 {
+    let isPullRequest = await utils.isPullRequest(github);
+    let branchName = await utils.getBranchName(github);
+    let repoName = await utils.getRepoName();
+    let projectKey = process.env.GITHUB_REPOSITORY.replace('/', '_');
+
     if(isPullRequest)
     {
         await exec.exec(`sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.projectName=${repoName} -Dsonar.organization=virto-commerce -Dsonar.login=${process.env.SONAR_TOKEN} -Dsonar.host.url=https://sonarcloud.io -Dsonar.pullrequest.base=\"${github.context.payload.pull_request.base.ref}\" -Dsonar.pullrequest.branch=\"${github.context.payload.pull_request.title}\" -Dsonar.pullrequest.key=\"${github.context.payload.pull_request.number}\" -Dsonar.pullrequest.github.repository=\"${process.env.GITHUB_REPOSITORY}\" -Dsonar.pullrequest.provider=GitHub -Dsonar.branch=${branchName}`);
