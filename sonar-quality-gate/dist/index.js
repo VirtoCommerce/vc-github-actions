8216777910598d733ea367530418390411409ea6
Alexander Morogov <asmorogov@gmail.com>
2020-10-09 11:18:16 +0200

    VP-5080: Add sonar-quality-gate action

---
+++ sonar-quality-gate/dist/index.js
@@ -0,0 +1,171 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+const http_1 = __importDefault(require("http"));
+const url_1 = __importDefault(require("url"));
+const fs_1 = __importDefault(require("fs"));
+const debug = require('debug')('sonarqube:verify:status');
+const REPORT_FILE = '.scannerwork/report-task.txt';
+const DEFAULT_DELAY = 5;
+module.exports.reportFile = REPORT_FILE;
+module.exports.checkQualityGateStatus = checkQualityGateStatus;
+function checkQualityGateStatus(login, password, sonarHost, projectKey) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const gateUrl = sonarHost +
+            '/api/qualitygates/project_status?projectKey=' +
+            projectKey;
+        return yield checkReportStatus(login, password).then((reportStatus) => __awaiter(this, void 0, void 0, function* () {
+            debug('reportStatus : ' + reportStatus);
+            console.log('Check the Quality gate ' + gateUrl);
+            const srvUrl = url_1.default.parse(gateUrl);
+            const options = {
+                host: srvUrl.hostname,
+                path: srvUrl.path
+            };
+            yield addAuthHeader(options, login, password);
+            return new Promise((resolve, reject) => {
+                const req = http_1.default.request(options, response => {
+                    if (response.statusCode !== 200) {
+                        console.error('Error requesting the Report status');
+                        reject('SonarQube replied the status code ' + response.statusCode);
+                    }
+                    else {
+                        let body = '';
+                        response.on('data', function (chunk) {
+                            body += chunk;
+                        });
+                        req.on('error', function (err) {
+                            console.error('Error requesting the Quality Gate status');
+                            reject(err);
+                        });
+                        response.on('end', function () {
+                            return __awaiter(this, void 0, void 0, function* () {
+                                yield processGateResponse(body, resolve, reject);
+                            });
+                        });
+                    }
+                });
+                req.end();
+            });
+        }));
+    });
+}
+function processGateResponse(body, resolve, reject) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const gateResponse = JSON.parse(body);
+        const status = gateResponse.projectStatus.status;
+        console.log('QUALITY GATE STATUS : ' + status);
+        if (status != 'OK' && status != 'WARN') {
+            console.error('QUALITY GATE HAS FAILED');
+            const errors = gateResponse.projectStatus.conditions
+                .filter((cond) => cond.status == 'ERROR')
+                .map((cond) => `[${cond.metricKey}]: ${cond.actualValue} ${cond.comparator} ${cond.errorThreshold}`)
+                .join(', ');
+            debug(errors);
+            reject(errors);
+        }
+        resolve('OK');
+    });
+}
+function checkReportStatus(login, password = '', delayBetweenChecksInSecs = DEFAULT_DELAY) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            var _a;
+            const reportInfo = fs_1.default.readFileSync(REPORT_FILE, 'utf8');
+            const taskUrl = (_a = reportInfo.match(/ceTaskUrl=(.*)/)) === null || _a === void 0 ? void 0 : _a[1];
+            if (taskUrl == null) {
+                throw new Error("");
+            }
+            console.log('Report Status Url : ' + taskUrl);
+            const srvUrl = url_1.default.parse(taskUrl);
+            const options = {
+                host: srvUrl.hostname,
+                path: srvUrl.path
+            };
+            yield addAuthHeader(options, login, password);
+            const req = http_1.default.request(options, response => {
+                if (response.statusCode !== 200) {
+                    console.error('Error requesting the Report status');
+                    reject('SonarQube replied the status code ' + response.statusCode);
+                }
+                else {
+                    let body = '';
+                    response.on('data', function (chunk) {
+                        body += chunk;
+                    });
+                    req.on('error', function (err) {
+                        console.error('Error requesting the Report status');
+                        reject(err);
+                    });
+                    response.on('end', function () {
+                        debug('Raw response from SonarQube API');
+                        debug('"' + body + '"');
+                        if (body === '') {
+                            resolve('');
+                        }
+                        else {
+                            const taskResponse = JSON.parse(body);
+                            resolve(taskResponse.task.status);
+                        }
+                    });
+                }
+            });
+            req.end();
+        })).then(status => {
+            console.log('Report status : ' + status);
+            if (status === '' || status === 'IN_PROGRESS' || status === 'PENDING') {
+                console.log('Retry until report ends in ' + delayBetweenChecksInSecs + 's');
+                return delay(delayBetweenChecksInSecs * 1000).then(() => {
+                    return checkReportStatus(login, password, delayBetweenChecksInSecs);
+                });
+            }
+            else {
+                return status;
+            }
+        });
+    });
+}
+function addAuthHeader(options, login, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let auth = null;
+        if (login !== '' && login !== undefined) {
+            debug('Authentication active');
+            auth = 'Basic ' + new Buffer(login + ':' + password).toString('base64');
+        }
+        else {
+            debug('No authentication active');
+        }
+        if (auth) {
+            options.headers = {
+                Authorization: auth
+            };
+        }
+    });
+}
+function delay(t) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, t);
+    });
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let login = core_1.default.getInput("login");
+        let password = core_1.default.getInput("password");
+        let sonarHost = core_1.default.getInput("sonarHost");
+        let projectKey = core_1.default.getInput("projectKey");
+        yield checkQualityGateStatus(login, password, sonarHost, projectKey);
+    });
+}
+run().catch(error => core_1.default.setFailed(error.message));
---
+++ sonar-quality-gate/src/index.ts
@@ -0,0 +1,173 @@
+import exec from '@actions/exec'
+import core from '@actions/core'
+import http from 'http';
+import url from 'url';
+import fs from 'fs';
+const debug = require('debug')('sonarqube:verify:status');
+
+const REPORT_FILE = '.scannerwork/report-task.txt';
+const DEFAULT_DELAY = 5;
+
+module.exports.reportFile = REPORT_FILE;
+module.exports.checkQualityGateStatus = checkQualityGateStatus;
+
+async function checkQualityGateStatus(login: string, password: string, sonarHost: string, projectKey: string) {
+  const gateUrl =
+    sonarHost +
+    '/api/qualitygates/project_status?projectKey=' +
+    projectKey;
+
+  return await checkReportStatus(login, password).then(async reportStatus => {
+    debug('reportStatus : ' + reportStatus);
+    console.log('Check the Quality gate ' + gateUrl);
+    const srvUrl = url.parse(gateUrl);
+
+    const options = {
+      host: srvUrl.hostname,
+      path: srvUrl.path
+    };
+    await addAuthHeader(options, login, password);
+    return new Promise((resolve, reject) => {
+      const req = http.request(options, response => {
+        if (response.statusCode !== 200) {
+          console.error('Error requesting the Report status');
+          reject('SonarQube replied the status code ' + response.statusCode);
+        } else {
+          let body = '';
+          response.on('data', function(chunk) {
+            body += chunk;
+          });
+          req.on('error', function(err) {
+            console.error('Error requesting the Quality Gate status');
+            reject(err);
+          });
+          response.on('end', async function() {
+            await processGateResponse(body, resolve, reject);
+          });
+        }
+      });
+      req.end();
+    });
+  });
+}
+
+async function processGateResponse(body: string, resolve: any, reject: any) {
+  const gateResponse = JSON.parse(body);
+  const status = gateResponse.projectStatus.status;
+  console.log('QUALITY GATE STATUS : ' + status);
+  if (status != 'OK' && status != 'WARN') {
+    console.error('QUALITY GATE HAS FAILED');
+    const errors = gateResponse.projectStatus.conditions
+      .filter((cond: { status: string; }) => cond.status == 'ERROR')
+      .map(
+        (        cond: { metricKey: any; actualValue: any; comparator: any; errorThreshold: any; }) =>
+          `[${cond.metricKey}]: ${cond.actualValue} ${cond.comparator} ${cond.errorThreshold}`
+      )
+      .join(', ');
+    debug(errors);
+    reject(errors);
+  }
+  resolve('OK');
+}
+
+// TODO : max checks ?
+/**
+ *
+ * @param {*} login login to the SonarQube API. Use it too for tokens. Let it empty or undefined if no authentication is required.
+ * @param {*} password password to the SonarQube API. Use empty value for tokens.
+ * @param {*} delayBetweenChecksInSecs Defaults to 5
+ */
+async function checkReportStatus(
+  login: string,
+  password: string = '',
+  delayBetweenChecksInSecs: number = DEFAULT_DELAY
+): Promise<any> {
+  return new Promise(async (resolve, reject) => {
+    const reportInfo = fs.readFileSync(REPORT_FILE, 'utf8');
+    const taskUrl = reportInfo.match(/ceTaskUrl=(.*)/)?.[1];
+    if(taskUrl == null)
+    {
+        throw new Error("")
+    }
+    console.log('Report Status Url : ' + taskUrl);
+    const srvUrl = url.parse(taskUrl);
+    const options = {
+      host: srvUrl.hostname,
+      path: srvUrl.path
+    };
+    await addAuthHeader(options, login, password);
+
+    const req = http.request(options, response => {
+      if (response.statusCode !== 200) {
+        console.error('Error requesting the Report status');
+        reject('SonarQube replied the status code ' + response.statusCode);
+      } else {
+        let body = '';
+        response.on('data', function(chunk) {
+          body += chunk;
+        });
+        req.on('error', function(err) {
+          console.error('Error requesting the Report status');
+          reject(err);
+        });
+        response.on('end', function() {
+          debug('Raw response from SonarQube API');
+          debug('"' + body + '"');
+          // on first call, API response might be empty
+          if (body === '') {
+            resolve('');
+          } else {
+            const taskResponse = JSON.parse(body);
+            resolve(taskResponse.task.status);
+          }
+        });
+      }
+    });
+    req.end();
+  }).then(status => {
+    console.log('Report status : ' + status);
+    if (status === '' || status === 'IN_PROGRESS' || status === 'PENDING') {
+      console.log(
+        'Retry until report ends in ' + delayBetweenChecksInSecs + 's'
+      );
+      return delay(delayBetweenChecksInSecs * 1000).then(() => {
+        return checkReportStatus(login, password, delayBetweenChecksInSecs);
+      });
+    } else {
+      return status;
+    }
+  });
+}
+
+async function addAuthHeader(options: http.RequestOptions, login: string, password: string) {
+  let auth = null;
+  if (login !== '' && login !== undefined) {
+    debug('Authentication active');
+    auth = 'Basic ' + new Buffer(login + ':' + password).toString('base64');
+  } else {
+    debug('No authentication active');
+  }
+  if (auth) {
+    options.headers = {
+      Authorization: auth
+    };
+  }
+}
+
+// Add delay in the promise chain
+function delay(t: number) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, t);
+  });
+}
+
+async function run(): Promise<void> {
+  let login = core.getInput("login");
+  let password = core.getInput("password");
+  let sonarHost = core.getInput("sonarHost");
+  let projectKey = core.getInput("projectKey");
+
+  await checkQualityGateStatus(login, password, sonarHost, projectKey);
+}
+
+run().catch(error => core.setFailed(error.message))
