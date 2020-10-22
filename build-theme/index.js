a96bf646851e65fc3846930920bafdc62066d0b8
Alexander Morogov <asmorogov@gmail.com>
2020-10-22 14:58:20 +0200

    VP-5655: Refactoring of build-theme action

--- build-theme/index.js
+++ build-theme/index.js
@@ -5,39 +5,6 @@ const utils = require('@krankenbro/virto-actions-lib');
 const path = require('path');
 const fs = require('fs');
 
-async function getCommitCount(baseBranch) {
-    try {
-        let output = '';
-        let err = '';
-
-        // These are option configurations for the @actions/exec lib`
-        const options = {};
-        options.listeners = {
-            stdout: (data) => {
-                output += data.toString();
-            },
-            stderr: (data) => {
-                err += data.toString();
-            }
-        };
-
-        await exec.exec(`git rev-list --count ${baseBranch}`, [], options).then(exitCode => console.log(`git rev-list --count exitCode: ${exitCode}`));
-        const commitCount = output.trim();
-
-        if (commitCount) {
-            console.log('\x1b[32m%s\x1b[0m', `${baseBranch} branch contain: ${commitCount} commits`);
-            result = commitCount;
-        } else {
-            core.setFailed(err);
-            process.exit(1);
-        }
-    } catch (err) {
-        core.setFailed(`Could not get commit counts because: ${err.message}`);
-        process.exit(1);
-    }
-    return result;
-}
-
 async function restoreDependencies()
 {
     await exec.exec('npm install');
@@ -56,20 +23,13 @@ async function run()
     let branchName = await utils.getBranchName(github);
     console.log(`Branch: ${branchName}`);
 
-// Alpha version should be created in any case except branchName is equal to releaseBranch
-    if(branchName !== releaseBranch)
-    {
-        let commitNumber = await getCommitCount(branchName);
-        versionSuffix = `-alpha.${commitNumber}`;
-        console.log(`Version suffix: ${versionSuffix}`);
-    }
-
     await restoreDependencies();
     await buildTheme();
 
 // Alpha version should be created in any case except branchName is equal to releaseBranch
     if(branchName !== releaseBranch)
     {
+        versionSuffix = core.getInput("versionSuffix");
         let versionPrefix = branchName.substring(branchName.lastIndexOf('/'),branchName.length).toLowerCase() + '-';
         let artifactPath = await utils.findArtifact("artifacts/*.zip");
         let artifactName = path.parse(artifactPath).name;
