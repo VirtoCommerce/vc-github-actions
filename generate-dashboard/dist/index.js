3f0cce73e150dec4125be0ce6a62ce814c901d6e
Alexander Morogov <asmorogov@gmail.com>
2020-11-10 09:33:43 +0200

    VP-5849: Chancge job_url to html_url

--- generate-dashboard/dist/index.js
+++ generate-dashboard/dist/index.js
@@ -65,7 +65,7 @@ function run() {
                     workflow_id: workflow.id,
                     per_page: 1
                 });
-                tableRow += `<a href="${(_a = runs.data.workflow_runs[0]) === null || _a === void 0 ? void 0 : _a.jobs_url}"><img src="${workflow.badge_url}" /></a>`;
+                tableRow += `<a href="${(_a = runs.data.workflow_runs[0]) === null || _a === void 0 ? void 0 : _a.html_url}"><img src="${workflow.badge_url}" /></a>`;
             }
             tableRow += "</td></tr>";
             table += tableRow;
