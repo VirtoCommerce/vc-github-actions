e3d312cb2e3ae017f7aee4c7aef845b76b4d3b12
Maksim Kopnov <mko@virtoway.com>
2021-01-29 22:21:20 +0700

    VDS-414: Create branch for deploy

--- get-artifact-link/src/index.ts
+++ get-artifact-link/src/index.ts
@@ -3,14 +3,19 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
     
-    const downloadComment = 'Download artifact URL:'
-    let GITHUB_TOKEN = core.getInput("githubToken");
+    
+    const GITHUB_TOKEN = core.getInput("githubToken");
     if(!GITHUB_TOKEN  && process.env.GITHUB_TOKEN !== undefined) GITHUB_TOKEN = process.env.GITHUB_TOKEN;
     
-    let repoOrg = core.getInput("repoOrg");
-    let octokit = github.getOctokit(GITHUB_TOKEN);
+    const downloadComment = 'Download artifact URL:'
+    const deployRepo = core.getInput("deployRepo");
+    const deployBranch = core.getInput("deployBranch");
+    const repoOrg = core.getInput("repoOrg");
+
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const regexp = RegExp(downloadComment + '\s*.*');
+    const regExpTask = /\w+-\d+/
 
     let currentPr = await octokit.pulls.get({
         owner: repoOrg,
@@ -18,7 +23,9 @@ async function run(): Promise<void> {
         pull_number: github.context.payload.pull_request?.number ?? github.context.issue.number
     });
 
+    let taskNumber = currentPr.data.title.match(regExpTask)?.[0];
     let body = currentPr.data.body;
+
     console.log(currentPr.data.title);
     console.log(body);
 
@@ -27,6 +34,19 @@ async function run(): Promise<void> {
 
     console.log(`Artifact link is: ${artifactLink}`); 
     core.setOutput('artifactLink', artifactLink);
+
+    const { data: baseBranch } = await octokit.git.getRef({
+        owner: repoOrg,
+        repo: deployRepo,
+        ref: `heads/${deployBranch}`
+    });
+
+    await octokit.git.createRef({
+        owner: repoOrg,
+        repo: deployRepo,
+        ref: `refs/heads/${taskNumber}`,
+        sha: baseBranch.object.sha,
+    });
 
 }
 
