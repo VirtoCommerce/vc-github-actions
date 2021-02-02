7db4b30051b7745a0dd6f543237fce3f8d6f28dd
Alexander Morogov <asmorogov@gmail.com>
2021-02-02 10:00:18 +0200

    VP-6710: Add support for yml  and json manidests

---
+++ docker-install-modules/dist/index.js
@@ -0,0 +1,80 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const exec = __importStar(require("@actions/exec"));
+const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
+const utils = __importStar(require("@virtocommerce/vc-actions-lib"));
+const yaml = __importStar(require("js-yaml"));
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let manifestUrl = core.getInput("manifestUrl");
+        let manifestFormat = core.getInput("manifestFormat");
+        let modulesGroup = core.getInput("modulesGroup");
+        let containerName = core.getInput("containerName");
+        let containerDestiantion = core.getInput("containerDestination");
+        let restartContainer = core.getInput("restartContainer") == 'true';
+        let manifestPath = `modules.${manifestFormat}`;
+        yield utils.downloadFile(manifestUrl, manifestPath);
+        let modulesJson;
+        let modulesDir = __dirname + 'Modules';
+        let modulesZipDir = __dirname + 'ModulesZip';
+        yield fs.mkdirSync(modulesDir);
+        yield fs.mkdirSync(modulesZipDir);
+        if (manifestFormat == 'json') {
+            let jsonString = fs.readFileSync(manifestPath, 'utf8');
+            let doc = JSON.parse(jsonString);
+            for (let module of doc) {
+                if (modulesGroup && !module['Groups'].includes(modulesGroup)) {
+                    continue;
+                }
+                for (let moduleVersion of module.Version) {
+                    if (moduleVersion.VersionTag) {
+                        continue;
+                    }
+                    let archivePath = path.join(modulesZipDir, `${module['Id']}.zip`);
+                    yield utils.downloadFile(moduleVersion['PackageUrl'], archivePath);
+                    yield exec.exec(`unzip ${archivePath} -d ${modulesDir}/${module['Id']}`);
+                }
+            }
+        }
+        else if (manifestFormat == 'yml') {
+            let jsonString = yaml.load(fs.readFileSync(manifestPath, 'utf8'));
+            let doc = JSON.parse(jsonString);
+            for (let module of doc['data']['modules.json']) {
+                let archivePath = path.join(modulesZipDir, `${module['Id']}.zip`);
+                yield utils.downloadFile(module['PackageUrl'], archivePath);
+                yield exec.exec(`unzip ${archivePath} -d ${modulesDir}/${module['Id']}`);
+            }
+        }
+    });
+}
+run().catch(error => core.setFailed(error.message));
--- docker-install-modules/src/index.ts
+++ docker-install-modules/src/index.ts
@@ -1,10 +1,57 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as path from 'path'
+import * as fs from 'fs'
 import * as utils from '@virtocommerce/vc-actions-lib'
+import * as yaml from 'js-yaml'
 
 async function run(): Promise<void> {
-    
+    let manifestUrl = core.getInput("manifestUrl");
+    let manifestFormat = core.getInput("manifestFormat");
+    let modulesGroup = core.getInput("modulesGroup");
+    let containerName = core.getInput("containerName");
+    let containerDestiantion = core.getInput("containerDestination");
+    let restartContainer = core.getInput("restartContainer") == 'true';
+
+    let manifestPath = `modules.${manifestFormat}`;
+    await utils.downloadFile(manifestUrl, manifestPath);
+    let modulesJson;
+    let modulesDir = __dirname + 'Modules';
+    let modulesZipDir = __dirname + 'ModulesZip'
+    await fs.mkdirSync(modulesDir);
+    await fs.mkdirSync(modulesZipDir);
+    if(manifestFormat == 'json')
+    {
+        let jsonString = fs.readFileSync(manifestPath, 'utf8');
+        let doc = JSON.parse(jsonString);
+        for(let module of doc)
+        {
+            if(modulesGroup && !module['Groups'].includes(modulesGroup))
+            {
+                continue;
+            }
+            for(let moduleVersion of module.Version)
+            {
+                if(moduleVersion.VersionTag)
+                {
+                    continue;
+                }
+                let archivePath = path.join(modulesZipDir, `${module['Id']}.zip`);
+                await utils.downloadFile(moduleVersion['PackageUrl'], archivePath);
+                await exec.exec(`unzip ${archivePath} -d ${modulesDir}/${module['Id']}`);
+            }
+        }
+    }
+    else if(manifestFormat == 'yml')
+    {
+        let jsonString = yaml.load(fs.readFileSync(manifestPath, 'utf8')) as string;
+        let doc = JSON.parse(jsonString);
+        for(let module of doc['data']['modules.json']){
+            let archivePath = path.join(modulesZipDir, `${module['Id']}.zip`);
+            await utils.downloadFile(module['PackageUrl'], archivePath);
+            await exec.exec(`unzip ${archivePath} -d ${modulesDir}/${module['Id']}`);
+        }
+    }
 }
 
 run().catch(error => core.setFailed(error.message));
