d371e71c7158aefaed91217bd6109f4d2b336660
Maksim Kopnov <mko@virtoway.com>
2021-02-02 14:39:43 +0700

    VP-6526: Update branch name in createOrUpdateFileContents

--- get-artifact-link/dist/index.js
+++ get-artifact-link/dist/index.js
@@ -149,8 +149,8 @@ function createDeployPr(deployData, targetRepo, baseRepo, octokit) {
                     return [4, octokit.pulls.create({
                             owner: targetRepo.repoOrg,
                             repo: targetRepo.repoName,
-                            head: targetRepo.branchName,
-                            base: targetBranchName,
+                            head: "refs/heads/" + targetRepo.branchName,
+                            base: "refs/heads/" + targetBranchName,
                             title: targetBranchName,
                             body: "Automated update " + baseRepo.repoName + " from PR " + baseRepo.pullNumber + " " + baseRepo.pullHtmlUrl
                         })];
