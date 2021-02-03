f6bf3fa540efeda5710e826648b2aeff71a6ddfa
Alexander Morogov <asmorogov@gmail.com>
2021-02-03 12:09:17 +0200

    VP-6710: Fix modules source path

--- docker-install-modules/dist/index.js
+++ docker-install-modules/dist/index.js
@@ -101,7 +101,7 @@ function run() {
                 yield exec.exec(`unzip ${archivePath} -d ${modulesDir}/${module['Id']}`);
             }
         }
-        yield exec.exec(`docker cp ./${modulesDir}/. ${containerName}:${containerDestination}`);
+        yield exec.exec(`docker cp ${modulesDir}/. ${containerName}:${containerDestination}`);
         if (restartContainer) {
             yield exec.exec(`docker restart ${containerName}`);
             yield sleep(sleepAfterRestart);
