d10591fdfb1670234227b57a5e530deeb4af0a7b
Alexander Morogov <asmorogov@gmail.com>
2021-02-05 11:34:00 +0200

    VP-6856: Add docker-start-environment action

---
+++ docker-start-environment/src/index.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
+
+
+// env:
+//   PLATFORM_IMAGE: ${{ inputs.platformImage }}
+//   STOREFRONT_IMAGE: ${{ inputs.storefrontImage }}
+//   PLATFORM_DOCKER_TAG: ${{ inputs.platformDockerTag }}
+//   STOREFRONT_DOCKER_TAG: ${{ inputs.storefrontDockerTag }}
+
+async function run(): Promise<void> {
+    let platformImage = core.getInput('platformImage');
+    let storefrontImage = core.getInput('storefrontImage');
+    let platformDockerTag = core.getInput('platformDockerTag');
+    let storefrontDockerTag = core.getInput('storefrontDockerTag');
+    let  envVarsArg = `-e PLATFORM_IMAGE=${platformImage} -e STOREFRONT_IMAGE=${storefrontImage} -e PLATFORM_DOCKER_TAG=${platformDockerTag} -e STOREFRONT_DOCKER_TAG=${storefrontDockerTag}`;
+
+    await exec.exec(`docker-compose up -d ${envVarsArg}`);
+}
+
+run().catch(error => core.setFailed(error.message));
