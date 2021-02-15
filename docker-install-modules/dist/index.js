8b2a98bebf2d7619a3424d92045f2103455a47c4
Alexander Morogov <asmorogov@gmail.com>
2021-02-15 17:14:59 +0200

    VP-6856: Fix dir creation

--- docker-install-modules/dist/index.js
+++ docker-install-modules/dist/index.js
@@ -127,8 +127,7 @@ function run() {
                 }
                 let moduleDstPath = `${modulesDir}/${module['Id']}`;
                 console.log(`${module['Id']}\n${moduleDstPath}`);
-                yield exec.exec(`mkdir ${moduleDstPath}`);
-                yield exec.exec(`unzip ${archivePath} -d ${moduleDstPath}`);
+                yield exec.exec(`unzip ${archivePath} -d \"${moduleDstPath}\"`);
             }
         }
         yield exec.exec('docker ps -a');
