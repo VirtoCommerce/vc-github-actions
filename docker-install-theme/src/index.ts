e4fd595abf00aa558d36c252cb4b28cd3fd8a7ac
Alexander Morogov <asmorogov@gmail.com>
2021-02-17 10:46:47 +0200

    VP-6856: Remove redundant message

--- docker-install-theme/src/index.ts
+++ docker-install-theme/src/index.ts
@@ -20,7 +20,6 @@ async function run(): Promise<void> {
     await exec.exec(`unzip ${artifactPath} -d ./${dirname}`);
     await exec.exec(`docker exec ${containerName} sh -c "mkdir -p ${containerDestination}"`)
     await exec.exec(`docker cp ./${dirname}/default/. ${containerName}:${containerDestination}`);
-    await exec.exec(`docker exec ${containerName} sh -c "ls -al /opt/virtocommerce/storefront/wwwroot/cms-content/Themes/Electronics/default"`);
     if(restartContainer)
     {
         await exec.exec(`docker restart ${containerName}`);
