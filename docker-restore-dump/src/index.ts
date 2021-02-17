20a90ff7972fe2b02c05dbdc597f6d585267a912
Alexander Morogov <asmorogov@gmail.com>
2021-02-17 09:12:37 +0200

    VP-6856: Add docker-restore-dump action

---
+++ docker-restore-dump/src/index.ts
@@ -0,0 +1,17 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import path from 'path'
+
+async function run(): Promise<void> {
+    let host = core.getInput('host');
+    let port = core.getInput('port');
+    let user = core.getInput('user');
+    let pass = core.getInput('pass');
+    let dumpUrl = core.getInput('dumpUrl');
+    let dumpFile = path.join(__dirname, 'dump.sql');
+
+    await exec.exec(`wget ${dumpUrl} -O ${dumpFile}`);
+    await exec.exec(`sqlcmd -S tcp:${host},${port} -U ${user} -P ${pass} -i ${dumpFile}`);
+}
+
+run().catch(error => core.setFailed(error.message));
