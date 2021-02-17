1549422504d01a8209482f9c596449869dabe0a3
Alexander Morogov <asmorogov@gmail.com>
2021-02-17 11:14:52 +0200

    VP-6856: Add debug message

--- docker-install-theme/src/index.ts
+++ docker-install-theme/src/index.ts
@@ -20,6 +20,7 @@ async function run(): Promise<void> {
     await exec.exec(`unzip ${artifactPath} -d ./${dirname}`);
     await exec.exec(`docker exec ${containerName} sh -c "mkdir -p ${containerDestination}"`)
     await exec.exec(`docker cp ./${dirname}/default/. ${containerName}:${containerDestination}`);
+    await exec.exec(`docker exec ${containerName} sh -c "ls -al ${containerDestination}"`);
     if(restartContainer)
     {
         await exec.exec(`docker restart ${containerName}`);
