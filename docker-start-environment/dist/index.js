3846ae60335641e42c1c1e89d865cade0cc4f565
Alexander Morogov <asmorogov@gmail.com>
2021-02-17 15:49:18 +0200

    VP-6856: Fix env variables

--- docker-start-environment/dist/index.js
+++ docker-start-environment/dist/index.js
@@ -48,6 +48,10 @@ function run() {
         let storefrontDockerTag = core.getInput('storefrontDockerTag');
         let composeProjectName = core.getInput('composeProjectName');
         let envFileContent = `PLATFORM_IMAGE=${platformImage}\nSTOREFRONT_IMAGE=${storefrontImage}\nPLATFORM_DOCKER_TAG=${platformDockerTag}\nSTOREFRONT_DOCKER_TAG=${storefrontDockerTag}`;
+        process.env.PLATFORM_IMAGE = platformImage;
+        process.env.PLATFORM_DOCKER_TAG = platformDockerTag;
+        process.env.STOREFRONT_IMAGE = storefrontImage;
+        process.env.STOREFRONT_DOCKER_TAG = storefrontDockerTag;
         fs_1.default.writeFileSync('./.env', envFileContent);
         let composePath = path_1.default.join(__dirname, '../docker-compose.yml');
         yield exec.exec(`docker-compose -p ${composeProjectName} -f ${composePath} --env-file ./.env up -d`);
--- docker-start-environment/src/index.ts
+++ docker-start-environment/src/index.ts
@@ -23,6 +23,10 @@ async function run(): Promise<void> {
     let storefrontDockerTag = core.getInput('storefrontDockerTag');
     let composeProjectName = core.getInput('composeProjectName');
     let envFileContent = `PLATFORM_IMAGE=${platformImage}\nSTOREFRONT_IMAGE=${storefrontImage}\nPLATFORM_DOCKER_TAG=${platformDockerTag}\nSTOREFRONT_DOCKER_TAG=${storefrontDockerTag}`;
+    process.env.PLATFORM_IMAGE = platformImage;
+    process.env.PLATFORM_DOCKER_TAG = platformDockerTag;
+    process.env.STOREFRONT_IMAGE = storefrontImage;
+    process.env.STOREFRONT_DOCKER_TAG = storefrontDockerTag;
     fs.writeFileSync('./.env', envFileContent);
     let composePath = path.join(__dirname, '../docker-compose.yml');
     await exec.exec(`docker-compose -p ${composeProjectName} -f ${composePath} --env-file ./.env up -d`);
