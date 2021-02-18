7fe1a0f0fd394425c52d1df6b9383f8edad5a885
Alexander Morogov <asmorogov@gmail.com>
2021-02-18 13:03:51 +0200

    VP-7021: Install vc-build beta 3

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.5.2-beta0002");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.5.2-beta0003");
 }
 
 run().catch(err => core.setFailed(err.message));
