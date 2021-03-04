dc64fe0376c703d4db9aab6ce45121561e689b10
Alexander Morogov <asmorogov@gmail.com>
2021-03-04 12:17:59 +0200

    VP-1985: Format ps1 file

--- update-virtocommercecom/ps/update-catalog.ps1
+++ update-virtocommercecom/ps/update-catalog.ps1
@@ -10,13 +10,11 @@ Param(
 
 . $PSScriptRoot\utilities.ps1   
 
-if ([string]::IsNullOrWhiteSpace($hmacAppId))
-{
+if ([string]::IsNullOrWhiteSpace($hmacAppId)) {
     $hmacAppId = "${env:HMAC_APP_ID}"
 }
 
-if ([string]::IsNullOrWhiteSpace($hmacSecret))
-{
+if ([string]::IsNullOrWhiteSpace($hmacSecret)) {
     $hmacSecret = "${env:HMAC_SECRET}"
 }     
 
@@ -27,35 +25,31 @@ $productCode = $moduleId.Replace(".", "_")
 # Initiate modules installation
 $headerValue = Create-Authorization $hmacAppId $hmacSecret
 $headers = @{
-    "Authorization" =  $headerValue
-    "content-type" = "application/json;charset=UTF-8"
-    "accept" = "application/json"
+    "Authorization" = $headerValue
+    "content-type"  = "application/json;charset=UTF-8"
+    "accept"        = "application/json"
 }
 
 $listEntriesBody = @{
-    "catalogId" = $catalogId
-    "categoryId" = $categoryId
-    "keyword" = $productCode
-    "responseGroup" = "withCategories, withProducts"
+    "catalogId"        = $catalogId
+    "categoryId"       = $categoryId
+    "keyword"          = $productCode
+    "responseGroup"    = "withCategories, withProducts"
     "searchVariations" = "false"
-    "skip" = 0
-    "sort" = "ASC"
+    "skip"             = 0
+    "sort"             = "ASC"
 } | ConvertTo-Json
 $result = Invoke-RestMethod -Uri $listEntriesUrl -Method Post -Headers $headers -Body $listEntriesBody
 
-foreach($entry in $result.listEntries)
-{
+foreach ($entry in $result.listEntries) {
     Write-Output $entry
-    if($entry.code -eq $productCode)
-    {    
+    if ($entry.code -eq $productCode) {    
         $productId = $entry.id
         $productInfo = Invoke-RestMethod -Uri "$productUrl/$productId" -Method Get -Headers $headers
 
         Write-Output "_________"
-        foreach($property in $productInfo.properties)
-        {
-            if($property.name -eq "DownloadLink")
-            {
+        foreach ($property in $productInfo.properties) {
+            if ($property.name -eq "DownloadLink") {
                 Write-Output $property.values
                 Write-Output $property.values[0].value
                 $property.values[0].value = $moduleUrl
