790b8eda8f4551af942874d252d36814ed86db52
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2021-03-16 14:24:50 +0700

    Fix Unable to locate executable file: vc-build.

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -4,6 +4,7 @@ const exec = require('@actions/exec');
 async function run()
 {
     await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.5.2-beta0006");
+    await exec.exec("export PATH="$PATH:~/.dotnet/tools"");
 }
 
 run().catch(err => core.setFailed(err.message));
