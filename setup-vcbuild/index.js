9126b46f3d7123ce153366614621d609a2ff6a0f
Alexander Morogov <asmorogov@gmail.com>
2021-03-23 17:46:43 +0200

    VPI-32: Setup vc-build 1.5.2-beta8

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.5.2-beta0007");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.5.2-beta0008");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
