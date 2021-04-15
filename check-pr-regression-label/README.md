70fd8dba09c1fd626342f16c796c92c53e080492
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2021-04-15 22:55:58 +0700

    PT-802: Add check-pr-regression-label action (#59)
    * PT-802: Add check-pr-regression-label action
    
    * PT-802: Add pullUrl output parameter
    
    * PT-802: Update prNubmber
    
    * PT-802: Update regular expression
    
    * PT-802: Delete debug message

---
+++ check-pr-regression-label/README.md
@@ -0,0 +1,38 @@
+# check-pr-regression-label
+
+Checks PR contain label for regression
+
+## inputs:
+
+### githubToken:
+
+GitHub token
+
+### label:
+
+PR label for regression
+
+### commitMessage:
+
+Latest squash commit message which should contain PR number looks like (#123)
+
+## outputs:
+
+### isLabeled:
+
+Flag describes that PR contain label
+
+### pullNumber:
+
+PR number labeled for regression
+
+## Example of usage
+
+```
+- name: Check regression label
+  id: regressLabel
+  uses: VirtoCommerce/vc-github-actions/check-pr-regression-label@master
+  with:
+    label: regression
+    commitMessage: ${{ github.event.head_commit.message }}
+```
---
+++ check-pr-regression-label/src/index.ts
@@ -0,0 +1,69 @@
+import * as github from '@actions/github'
+import * as core from '@actions/core'
+
+interface Label
+{
+    id: number,
+    node_id: string,
+    url: string,
+    name: string,
+    description: string,
+    color: string,
+    default: boolean
+
+}
+
+function getPrNumber (commitMessage: string) {
+
+    console.log('Get PR number from commit message');
+
+    const regExpPr = /\(#\d+\)/;
+
+    let result = commitMessage.match(regExpPr)?.[0].match(/\d+/)?.[0];
+
+    return result;
+}
+
+async function run(): Promise<void> {
+    
+    let GITHUB_TOKEN = core.getInput("githubToken");
+    if(!GITHUB_TOKEN  && process.env.GITHUB_TOKEN !== undefined) GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+    const prLabel = core.getInput("label");
+    const squashCommitMessage = core.getInput("commitMessage");
+    const prNumber = getPrNumber(squashCommitMessage);
+
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    let isPrLabeled: boolean = false;
+    let prUrl: string = '';
+
+    //Get base PR
+    if (typeof prNumber !== 'undefined' ){
+
+        const { data: basePrData } = await octokit.pulls.get({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: Number(prNumber)
+        });
+    
+        if (typeof basePrData !== 'undefined' ){
+    
+            let labels = basePrData.labels as Label[];
+            isPrLabeled = labels.some( x  => x.name === prLabel)
+            prUrl = basePrData.html_url;
+        }
+    }
+
+    core.setOutput("pullNumber", prNumber);
+    core.setOutput("pullUrl", prUrl);
+    core.setOutput("isLabeled", isPrLabeled);
+
+    console.log(`Squashed PR number is : ${prNumber}`);
+    console.log(`PR link is : ${prUrl}`);
+    console.log(`PR is contain ${prLabel} label: ${isPrLabeled}`);
+
+}
+
+run().catch(error => core.setFailed(error.message));
+
