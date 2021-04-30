586a1691160393b18f6ceef01010a54a716f6814
Alexander Morogov <asmorogov@gmail.com>
2021-04-30 09:02:25 +0200

    VCI-36: Increase pause before auth

--- docker-env/scripts/check-installed-modules.ps1
+++ docker-env/scripts/check-installed-modules.ps1
@@ -26,7 +26,7 @@ function Get-AuthToken {
     return $responseContent.access_token
 }
 
-Start-Sleep -Seconds 120
+Start-Sleep -Seconds 180
 
 $authToken = (Get-AuthToken $appAuthUrl $Username $Password)[1]
 $headers = @{}
