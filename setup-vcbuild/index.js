bf39bd8cac5d3881f661c541d23c87b7293ccc27
Alexander Morogov <asmorogov@gmail.com>
2021-05-04 16:33:46 +0200

    PT-1586: Setup vc-build 1.7.2-beta0002

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.7.2-beta0001");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.7.2-beta0002");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
