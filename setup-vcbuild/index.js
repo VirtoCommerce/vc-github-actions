37a02add6ece5b49037cc2515c3b33181589387b
Alexander Morogov <asmorogov@gmail.com>
2021-05-04 14:21:03 +0200

    PT-1586: Setup previous version of sonarscanner

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,6 +3,8 @@ const exec = require('@actions/exec');
 
 async function run()
 {
+    await exec.exec("dotnet tool uninstall -g dotnet-sonarscanner");
+    await exec.exec("dotnet tool install --global dotnet-sonarscanner --version 5.2.0")
     await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 1.7.1-beta0002");
     core.addPath('/home/runner/.dotnet/tools/');
 }
