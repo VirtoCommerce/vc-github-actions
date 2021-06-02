bba443224bc8a34e97ad6374ec8bd71924ebe617
Maksim <mko@virtoway.com>
2021-06-02 11:58:54 +0700

    PT-2023: Fix

--- get-pr-labels/index.js
+++ get-pr-labels/index.js
@@ -1,8 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const DEPENDENCIES_LABEL = 'dependencies';
+
 async function run()
 {
     console.log(github.context.payload.pull_request.labels);
+    if (
+        context.payload.pull_request.labels.some(
+          (label) => label.name === DEPENDENCIES_LABEL
+        )
+      ){
+        console.log(`Pull request contain "${DEPENDENCIES_LABEL}", SonarScanner steps skipped`);
+    } else {
+        
+    }
 }
 run().catch(err => core.setFailed(err.message));
