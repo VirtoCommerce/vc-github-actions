e94ec8ac277de1a5f2fbf9dbc06871e1a4907bd2
Alexander Morogov <asmorogov@gmail.com>
2021-06-24 16:35:35 +0200

    PT-2060: Create an empty coverage report if it doesn't  exists

--- sonar-scanner-end/index.js
+++ sonar-scanner-end/index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 const utils = require('@virtocommerce/vc-actions-lib');
+const fs = require('fs');
 
 async function run()
 {
@@ -11,7 +12,11 @@ async function run()
          console.log(`Pull request contain "dependencies" label, SonarScanner steps skipped.`);
          return;
     }
-
+    const emptyCoverageContent = '<coverage version="1"></coverage>';
+    const coveragePath = './.tmp/coverage.xml'
+    if(!fs.existsSync(coveragePath)){
+        fs.writeFileSync(coveragePath, emptyCoverageContent);
+    }
     delete process.env.JAVA_TOOL_OPTIONS;
     await exec.exec(`vc-build SonarQubeEnd -SonarAuthToken ${process.env.SONAR_TOKEN} -skip`)
 }
