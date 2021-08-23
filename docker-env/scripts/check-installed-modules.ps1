cb76d4e0443a392898d8d5adcdac91515a1534ca
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2021-08-23 14:44:03 +0700

    VCI-130: Install modules step refactoring (#78)
    * VCI-130: Add wait-url-up function
    
    * VCI-130: Update check-installed-modules script
    
    * VCI-130: Setup modules by vc-build
    
    * VCI-130: Use tag for versioning
    
    * VCI-130: Fix Docker Environment + E2E workflow

--- docker-env/scripts/check-installed-modules.ps1
+++ docker-env/scripts/check-installed-modules.ps1
@@ -2,9 +2,11 @@ Param(
     [parameter(Mandatory = $true)]
     $ApiUrl,
     $Username = "admin",
-    $Password = "store"
+    $Password = "store",
+    $ContainerId = "virtocommerce_vc-platform-web_1"
 )
 
+. "./scripts/watch-url-up.ps1"
 
 $appAuthUrl = "$ApiUrl/connect/token"
 $checkModulesUrl = "$ApiUrl/api/platform/modules"
@@ -26,28 +28,35 @@ function Get-AuthToken {
     return $responseContent.access_token
 }
 
-Start-Sleep -Seconds 180
+Write-Host "Container $ContainerId restarted"
+docker restart $ContainerId
+docker logs $ContainerId
 
-$authToken = (Get-AuthToken $appAuthUrl $Username $Password)[1]
-$headers = @{}
-$headers.Add("Authorization", "Bearer $authToken")
-$modules = Invoke-RestMethod $checkModulesUrl -Method Get -Headers $headers -SkipCertificateCheck -MaximumRetryCount 5 -RetryIntervalSec 5
-$installedModules = 0
-if ($modules.Length -le 0) {
-    Write-Output "No module's info returned"
-    exit 1
-}
-Foreach ($module in $modules) {
-    if ($module.isInstalled) {
-        $installedModules++
+$platformIsUp = (Watch-Url-Up -ApiUrl $ApiUrl -TimeoutMinutes 15 -RetrySeconds 15 -WaitSeconds 60)
+
+if ($platformIsUp) {
+    $authToken = (Get-AuthToken $appAuthUrl $Username $Password)[1]
+    $headers = @{}
+    $headers.Add("Authorization", "Bearer $authToken")
+    $modules = Invoke-RestMethod $checkModulesUrl -Method Get -Headers $headers -SkipCertificateCheck -MaximumRetryCount 5 -RetryIntervalSec 5
+    $installedModules = 0
+    if ($modules.Length -le 0) {
+        Write-Output "No module's info returned"
+        exit 1
     }
-    if ($module.validationErrors.Length -gt 0) {
-        Write-Output $module.id
-        Write-Output $module.validationErrors
+    Foreach ($module in $modules) {
+        if ($module.isInstalled) {
+            $installedModules++
+        }
+        if ($module.validationErrors.Length -gt 0) {
+            Write-Output $module.id
+            Write-Output $module.validationErrors
+            exit 1
+        }
+    }
+    Write-Output "Modules installed: $installedModules"
+    if($installedModules -lt 23){
         exit 1
     }
 }
-Write-Output "Modules installed: $installedModules"
-if($installedModules -lt 23){
-    exit 1
-}
+
---
+++ docker-env/scripts/watch-url-up.ps1
@@ -0,0 +1,45 @@
+
+function Watch-Url-Up {
+    param 
+    (
+        [string]$ApiUrl = "http://localhost:8090", # Host URL
+        [int]$TimeoutMinutes = 15,                 # Max period of time for retry attempts in minutes
+        [int]$RetrySeconds = 15,                   # Period of time between retry attempts in seconds
+        [int]$WaitSeconds = 60                     # Period of time before start retry attempts in seconds
+    )
+
+    $responseStatus = 0
+    [int]$maxRepeat = $TimeoutMinutes * 60 / $RetrySeconds
+
+    Write-Host "Wait before start attempts for $WaitSeconds seconds"
+    Start-Sleep -s $WaitSeconds
+
+    $attempt = 1
+    $responseStatus = 0
+    do 
+    {
+        
+        Write-Host "Try to open $ApiUrl. Attempt # $attempt of $maxRepeat."
+        try {
+            $response = Invoke-WebRequest $ApiUrl -Method Get
+            $responseStatus = [int] $response.StatusCode
+        }
+        catch{
+            if ($maxRepeat -gt $attempt) {
+                Start-Sleep -s $RetrySeconds
+            }
+            $attempt ++
+        }
+    } until ($responseStatus -eq 200 -or $maxRepeat -lt $attempt)
+
+    if ($responseStatus -eq 200) {
+        Write-Host "$ApiUrl is up!"
+        $result = $true
+    }
+    else {
+        Write-Host "$ApiUrl may be down, please check!"
+        $result = $false
+    }
+
+    return $result
+}
