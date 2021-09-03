db6463ced524674d0f64bf4835c7c312a5176781
Alexander Morogov <asmorogov@gmail.com>
2021-09-03 15:06:37 +0200

    VCI-52: Add env preparing

--- get-env-from-deployment/dist/index.js
+++ get-env-from-deployment/dist/index.js
@@ -54,12 +54,157 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core = __importStar(require("@actions/core"));
-function run() {
+var exec = __importStar(require("@actions/exec"));
+var path = __importStar(require("path"));
+var fs = __importStar(require("fs"));
+var yaml = __importStar(require("yaml"));
+var axios_1 = __importDefault(require("axios"));
+function downloadFile(url, outFile) {
+    return __awaiter(this, void 0, void 0, function () {
+        var path, writer, response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    path = outFile;
+                    writer = fs.createWriteStream(path);
+                    return [4, (0, axios_1.default)({
+                            url: url,
+                            method: 'GET',
+                            responseType: 'stream',
+                            headers: {
+                                'Accept': 'application/octet-stream'
+                            }
+                        })];
+                case 1:
+                    response = _a.sent();
+                    response.data.pipe(writer);
+                    return [2, new Promise(function (resolve, reject) {
+                            writer.on('finish', resolve);
+                            writer.on('error', reject);
+                        })];
+            }
+        });
+    });
+}
+function getModules(modules, tmpDir, modulesDir) {
+    return __awaiter(this, void 0, void 0, function () {
+        var _i, modules_1, module_1, archivePath, packageUrl, isBlob, moduleRepo, moduleDstPath;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _i = 0, modules_1 = modules;
+                    _a.label = 1;
+                case 1:
+                    if (!(_i < modules_1.length)) return [3, 5];
+                    module_1 = modules_1[_i];
+                    if (module_1['Id'] == "VirtoCommerce.PageBuilderModule")
+                        return [3, 4];
+                    archivePath = path.join(tmpDir, module_1['Id'] + ".zip");
+                    packageUrl = module_1['PackageUrl'];
+                    isBlob = packageUrl.includes("windows.net/");
+                    moduleRepo = module_1['Repository'].split('/').pop();
+                    console.log(packageUrl);
+                    return [4, downloadFile(packageUrl, archivePath)];
+                case 2:
+                    _a.sent();
+                    moduleDstPath = modulesDir + "/" + module_1['Id'];
+                    console.log(module_1['Id'] + "\n" + moduleDstPath);
+                    return [4, extractArchive(archivePath, moduleDstPath)];
+                case 3:
+                    _a.sent();
+                    _a.label = 4;
+                case 4:
+                    _i++;
+                    return [3, 1];
+                case 5: return [2];
+            }
+        });
+    });
+}
+function extractArchive(zip, out) {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
-            return [2];
+            switch (_a.label) {
+                case 0: return [4, exec.exec("unzip " + zip + " -d \"" + out + "\"")];
+                case 1:
+                    _a.sent();
+                    return [2];
+            }
+        });
+    });
+}
+function run() {
+    return __awaiter(this, void 0, void 0, function () {
+        var platformDir, modulesDir, storefrontDir, themeDir, manifestPath, manifestUrl, tmpDir, rawContent, parsed, i, json, _i, parsed_1, p, j, parsedJSON, platformUrl, platformZip, modules, storefrontUrl, storefrontZip, themeUrl, themeZip;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    platformDir = core.getInput("platformDir");
+                    modulesDir = core.getInput("modulesDir");
+                    storefrontDir = core.getInput("storefrontDir");
+                    themeDir = core.getInput("themeDir");
+                    manifestPath = "./modules.yaml";
+                    manifestUrl = core.getInput("deploymentUrl");
+                    return [4, downloadFile(manifestUrl, manifestPath)];
+                case 1:
+                    _a.sent();
+                    tmpDir = path.join(__dirname, 'tmp');
+                    return [4, fs.mkdirSync(tmpDir)];
+                case 2:
+                    _a.sent();
+                    rawContent = fs.readFileSync(manifestPath);
+                    parsed = yaml.parseAllDocuments(rawContent.toString());
+                    i = 0;
+                    for (_i = 0, parsed_1 = parsed; _i < parsed_1.length; _i++) {
+                        p = parsed_1[_i];
+                        j = p.toJSON();
+                        if (j.hasOwnProperty("data")) {
+                            if (j["data"].hasOwnProperty("modules.json")) {
+                                json = j;
+                                break;
+                            }
+                        }
+                    }
+                    parsedJSON = JSON.parse(json);
+                    console.log('Download platform');
+                    platformUrl = parsedJSON['data']['PLATFORM_URL'];
+                    platformZip = path.join(tmpDir, 'platform.zip');
+                    return [4, downloadFile(platformUrl, platformZip)];
+                case 3:
+                    _a.sent();
+                    return [4, extractArchive(platformZip, platformDir)];
+                case 4:
+                    _a.sent();
+                    console.log('Download modules');
+                    modules = parsedJSON['data']['modules.json'];
+                    return [4, getModules(modules, tmpDir, modulesDir)];
+                case 5:
+                    _a.sent();
+                    console.log('Download storefront');
+                    storefrontUrl = parsedJSON['data']['STOREFRONT_URL'];
+                    storefrontZip = path.join(tmpDir, 'storefront.zip');
+                    return [4, downloadFile(storefrontUrl, storefrontZip)];
+                case 6:
+                    _a.sent();
+                    return [4, extractArchive(storefrontZip, storefrontDir)];
+                case 7:
+                    _a.sent();
+                    console.log('Download theme');
+                    themeUrl = parsedJSON['data']['THEME_URL'];
+                    themeZip = path.join(tmpDir, 'theme.zip');
+                    return [4, downloadFile(themeUrl, themeZip)];
+                case 8:
+                    _a.sent();
+                    return [4, extractArchive(themeZip, themeDir)];
+                case 9:
+                    _a.sent();
+                    return [2];
+            }
         });
     });
 }
--- get-env-from-deployment/src/index.ts
+++ get-env-from-deployment/src/index.ts
@@ -4,9 +4,99 @@ import * as github from '@actions/github'
 import * as path from 'path'
 import * as fs from 'fs'
 import * as yaml from 'yaml'
+import Axios from 'axios'
+import { tmpdir } from 'os'
+
+async function downloadFile(url: string, outFile: string) {
+    const path = outFile;
+    const writer = fs.createWriteStream(path);
+  
+    const response = await Axios({
+      url,
+      method: 'GET',
+      responseType: 'stream',
+      headers: {
+          'Accept': 'application/octet-stream'
+      }
+    })
+    
+    response.data.pipe(writer)
+  
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve)
+      writer.on('error', reject)
+    })
+}
+
+async function getModules(modules: any, tmpDir: string, modulesDir: string) {
+    
+    for(let module of modules){
+        if(module['Id'] == "VirtoCommerce.PageBuilderModule")
+            continue;
+        let archivePath = path.join(tmpDir, `${module['Id']}.zip`);
+        let packageUrl = module['PackageUrl'] as string;
+        let isBlob = packageUrl.includes("windows.net/");
+        let moduleRepo = module['Repository'].split('/').pop() as string;
+        console.log(packageUrl);
+        await downloadFile(packageUrl, archivePath);
+        let moduleDstPath = `${modulesDir}/${module['Id']}`;
+        console.log(`${module['Id']}\n${moduleDstPath}`);
+        await extractArchive(archivePath, moduleDstPath);
+    }
+}
+
+async function extractArchive(zip:string, out: string) {
+    await exec.exec(`unzip ${zip} -d \"${out}\"`);
+}
 
 async function run(): Promise<void> {
-    
+    let platformDir = core.getInput("platformDir");
+    let modulesDir = core.getInput("modulesDir");
+    let storefrontDir = core.getInput("storefrontDir");
+    let themeDir = core.getInput("themeDir");
+    let manifestPath = `./modules.yaml`;
+    let manifestUrl = core.getInput("deploymentUrl");
+    await downloadFile(manifestUrl, manifestPath);
+
+    let tmpDir = path.join(__dirname, 'tmp');
+    await fs.mkdirSync(tmpDir);
+
+    let rawContent = fs.readFileSync(manifestPath);
+    let parsed = yaml.parseAllDocuments(rawContent.toString());
+    let i = 0;
+    let json: any;
+    for(let p of parsed){
+        let j = p.toJSON();
+        if(j.hasOwnProperty("data")){
+            if(j["data"].hasOwnProperty("modules.json")){
+                json = j;
+                break;
+            }
+        }
+    }
+    let  parsedJSON = JSON.parse(json);
+    //platform
+    console.log('Download platform');
+    let platformUrl = parsedJSON['data']['PLATFORM_URL'];
+    let platformZip = path.join(tmpDir, 'platform.zip');
+    await downloadFile(platformUrl, platformZip);
+    await extractArchive(platformZip, platformDir);
+    //modules
+    console.log('Download modules');
+    let modules = parsedJSON['data']['modules.json'];
+    await getModules(modules, tmpDir, modulesDir);
+    //storefront
+    console.log('Download storefront');
+    let storefrontUrl = parsedJSON['data']['STOREFRONT_URL'];
+    let storefrontZip = path.join(tmpDir, 'storefront.zip');
+    await downloadFile(storefrontUrl, storefrontZip);
+    await extractArchive(storefrontZip, storefrontDir);
+    //theme
+    console.log('Download theme');
+    let themeUrl = parsedJSON['data']['THEME_URL'];
+    let themeZip = path.join(tmpDir, 'theme.zip');
+    await downloadFile(themeUrl, themeZip);
+    await extractArchive(themeZip, themeDir);
 }
 
 run().catch(error => core.setFailed(error.message));
