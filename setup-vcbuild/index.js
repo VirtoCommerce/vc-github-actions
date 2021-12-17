44e02bd39f17a380c26f01c82c7ade3caba82a94
Alexander Morogov <asmorogov@gmail.com>
2021-12-17 12:16:09 +0200

    VCI-237: Update vc-build to 2.2.0-alpha.28

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 2.2.0-alpha.27");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 2.2.0-alpha.28");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
