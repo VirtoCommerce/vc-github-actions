fb9bcb555ffb568337411f2dedb4c832f5455256
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-03-22 17:48:00 +0700

    PT-6254: Add downloadUrl output parameter for publish-github-release action (#123)
    * PT-6254: Delete node_modules from repo
    
    * PT-6254: Add downloadUrl output
    
    * PT-6254: Fix module workflow

--- publish-github-release/src/index.ts
+++ publish-github-release/src/index.ts
@@ -2,6 +2,7 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as fs from 'fs'
+import path from 'path'
 import * as utils from '@virtocommerce/vc-actions-lib'
 
 async function installGithubRelease()
@@ -11,6 +12,31 @@ async function installGithubRelease()
     process.env.PATH = `${process.env.PATH}:${process.env.HOME}/go/bin`;
     console.log(process.env.PATH);
     console.log(process.env.HOME);
+}
+
+async function getDownloadUrl() {
+    
+    const version = core.getInput("version");
+    let result = "";
+    if (version) {
+        const ownerName = github.context.repo.owner;
+        const repoName = github.context.repo.repo;
+        const artifactPath = await utils.findArtifact("artifacts/*.zip");
+        console.log(artifactPath);
+        
+        const artifactFileName = artifactPath.split(path.sep).pop();
+        console.log(artifactFileName);
+    
+        const downloadUrl = `https://github.com/${ownerName}/${repoName}/releases/download/${version}/${artifactFileName}`;
+        console.log(`Download url: ${downloadUrl}`);
+
+        result = downloadUrl;
+
+    } else {
+        console.log("\x1b[33mWarning!\x1b[0m The download URL could not be generated because the version number is empty.")
+    }
+
+    return result;
 }
 
 async function run()
@@ -31,6 +57,8 @@ async function run()
             console.log(`vc-build Release exit code: ${exitCode}`);
         }
     });
+    const downloadUrl = await getDownloadUrl();
+    core.setOutput('downloadUrl',downloadUrl);
 }
 
 run().catch(error => {
