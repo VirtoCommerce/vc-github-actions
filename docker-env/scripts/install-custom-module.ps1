2df03a6fa557b3f5f05e23ac170bea3d2438a213
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-04-28 18:29:55 +0700

    PT-7271: fix

---
+++ docker-env/scripts/install-custom-module.ps1
@@ -0,0 +1,32 @@
+Set-Variable -Name "TERM" -Value "xterm-color"
+
+function InstallCustomModule {
+    param (
+        [string]$InstallFolder = "modules", # Folder where vc-package.json placed
+        [string]$CustomModuleId, # CustomModuleId to reinstall
+        [string]$CustomModuleUrl
+    )
+
+    Write-Host "`e[33mInstall Custom Modules step started."
+    $CustomModuleZip = "./$($CustomModuleId)"
+    Push-Location "./$($InstallFolder)"
+    Write-Host "`e[33mTry to uninstall $($CustomModuleId)."
+    try {
+        vc-build uninstall -module $($CustomModuleId)
+        Write-Host "`e[32m$($CustomModuleId) successfull uninstalled."
+    }
+    catch {
+        Write-Host "`e[33mError ocure while $($CustomModuleId) uninstall."
+    }
+    Pop-Location
+    Push-Location "./$($InstallFolder)/modules"
+    Write-Host "`e[33mDownload $($CustomModuleUrl) to $($CustomModuleZip)."
+    Invoke-WebRequest -Uri $CustomModuleUrl -OutFile $CustomModuleZip
+    Write-Host "`e[33mExpand $($CustomModuleZip) from zip."
+    Expand-Archive $CustomModuleZip -Force
+    Write-Host "`e[33mDelete $($CustomModuleZip)."
+    Remove-Item -Path $CustomModuleZip
+    Write-Host "`e[32m$($CustomModuleZip) deleted."
+    Pop-Location
+    Write-Host "`e[32mCustom module installed."
+}
