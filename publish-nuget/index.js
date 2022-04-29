41af25ec14f2d8742e8a66c9fbadd52309505a3c
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-04-29 15:51:04 +0700

    Add skip parameter for publish nuget action

--- publish-nuget/index.js
+++ publish-nuget/index.js
@@ -1,12 +1,10 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
-const utils = require('@virtocommerce/vc-actions-lib');
 
 async function run()
 {
-    let branchName = await utils.getBranchName(github);
-    await exec.exec(`vc-build PublishPackages -ApiKey ${process.env.NUGET_KEY} -skip Clean+Restore+Compile+Test`, [], { ignoreReturnCode: true, failOnStdErr: false }).then(exitCode => {
+    const skip = core.getInput('skipString');
+    await exec.exec(`vc-build PublishPackages -ApiKey ${process.env.NUGET_KEY} -skip ${skip}`, [], { ignoreReturnCode: true, failOnStdErr: false }).then(exitCode => {
         if(exitCode != 0 && exitCode != 409)
         {
             core.setFailed("Failed to publish nugets");
