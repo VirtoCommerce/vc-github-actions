9228867c7d3a4b780590d56cd6e6236f8b2f5cb3
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-05-31 12:01:44 +0700

    Fix

--- docker-env/scripts/install-custom-module.ps1
+++ docker-env/scripts/install-custom-module.ps1
@@ -12,8 +12,8 @@ function InstallCustomModule {
     Push-Location "./$($InstallFolder)"
     Write-Host "`e[33mTry to uninstall $($CustomModuleId)."
     try {
-#        & "vc-build uninstall -module $($CustomModuleId)"
-        & vc-build uninstall -module $($CustomModuleId)
+        & "vc-build uninstall -module $($CustomModuleId)"
+#        & vc-build uninstall -module $($CustomModuleId)
         Write-Host "`e[32m$($CustomModuleId) successfull uninstalled."
     }
     catch {
