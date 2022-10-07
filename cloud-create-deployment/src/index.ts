ef7054167c31e31fe128e4b7c2b4454e709103b6
Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
2022-10-07 11:33:23 +0700

    fix branch naming in cloud deployment

--- cloud-create-deployment/src/index.ts
+++ cloud-create-deployment/src/index.ts
@@ -39,7 +39,7 @@ async function createDeployPr(deployData: DeploymentData, targetRepo: RepoData, 
     console.log('Create deployment PR');
 
     const octokit = github.getOctokit(githubToken);
-    const targetBranchName = `${targetRepo.branchName}-deployment-${targetRepo.taskNumber}`;
+    const targetBranchName = `pr-${targetRepo.branchName}-${targetRepo.taskNumber}`;
 
     console.log(`Get ${targetRepo.branchName} base branch data`);
     //Get base branch data
