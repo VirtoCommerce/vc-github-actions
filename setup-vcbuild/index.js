b5a9a501e70f59f063f9214ccb1ddfc28c33996e
Oleg Zhuk <zhukoo@gmail.com>
2022-12-01 16:36:37 +0200

    Update vc-build to 3.10.0-alpha.99

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.6.0-alpha.88");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.10.0-alpha.99");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
