d4ad98010bb693335cd72e0da0be4d2627d16436
Oleg Zhuk <zhukoo@gmail.com>
2023-02-17 12:31:24 +0200

    Test retry policy for Katalon Studio execution

--- katalon-studio-github-action/index.js
+++ katalon-studio-github-action/index.js
@@ -1,38 +1,44 @@
 const { execute } = require("katalon-cli/src/katalon-studio");
 const core = require("@actions/core");
-const exec = require("@actions/exec");
 
 const user_version = core.getInput("version");
 const user_projectPath = core.getInput("projectPath");
 const user_args = core.getInput("args");
-const xvfbConf = core.getInput("xvfbConfiguration");
 
-try {
-  // let displays = exec.exec("xrandr --query");
-  // if(user_args.indexOf("-webui.autoUpdateDrivers") == -1){
-  //   user_args = `${user_args} -webui.autoUpdateDrivers=true`;
-  // }
-  execute(user_version, "", user_projectPath, user_args, "", "--auto-servernum --server-args=\"-screen 0 1920x1080x24\"", { // -n 99 --server-args="-screen 99 1920x1080x24
-    info: function (message) {
-      console.log(message);
-    },
-    debug: function (message) {
-      console.log(message);
-    },
-    error: function (message) {
-      console.error(message);
-    },
-  })
-    .then((status) => {
-      if (status !== 0) {
-        core.setFailed(`Exit code ${status}.`);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      core.setFailed(err);
-    });
-} catch (error) {
-  console.error(error);
-  core.setFailed(error.message);
+const retryCount = 3;
+const retryTimeout = 600000;
+
+async function run() {
+    for (let i = 0; i < retryCount; i++) {
+        try {
+            const status = await execute(user_version, "", user_projectPath, user_args, "", "--auto-servernum --server-args=\"-screen 0 1920x1080x24\"", {
+                info: function (message) {
+                    console.log(message);
+                },
+                debug: function (message) {
+                    console.log(message);
+                },
+                error: function (message) {
+                    console.error(message);
+                },
+            });
+
+            if (status === 0) {
+                console.log("Katalon Studio execution succeeded.");
+                return;
+            } else if (status === 3 && i < retryCount - 1) {
+                console.log("Katalon Studio execution failed with status 3. Retrying...");
+                await new Promise(resolve => setTimeout(resolve, retryTimeout));
+            } else {
+                core.setFailed(`Katalon Studio execution failed with status ${status}.`);
+                return;
+            }
+        } catch (err) {
+            console.error(err);
+            core.setFailed(err);
+            return;
+        }
+    }
 }
+
+run();
