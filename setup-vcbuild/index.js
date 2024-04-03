fc9666f17465567e35f8483a3c393c82555a6f5b
Oleg Zhuk <zhukoo@gmail.com>
2024-04-03 13:58:01 +0200

    Revert back to 3.21.5

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.800.0-alpha.192");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.21.5");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
