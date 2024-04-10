f41310662174e8bbeb163f073975a616648e8c64
Alexander Morogov <42555001+krankenbro@users.noreply.github.com>
2024-04-10 15:52:39 +0200

    Update vc-build

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.800.0-alpha.194");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.800.0-alpha.195");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
