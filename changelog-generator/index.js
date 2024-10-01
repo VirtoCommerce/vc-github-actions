ec7455024c1b557c26a226389b03434e3de41e34
Andrew Kubyshkin <andrew.kubyshkin@virtoway.com>
2024-10-01 18:35:17 +0500

    VCST-1835: Add check for empty body

--- changelog-generator/index.js
+++ changelog-generator/index.js
@@ -52,7 +52,8 @@ function cleanMessages(messages)
 
             const msgAndBody = oneLineMsg.split('BODY:');
             const msg = msgAndBody[0].split('MSG:')[1].trim();
-            const body = msgAndBody[1].trim();
+            // const body = msgAndBody[1].trim();
+            const body = msgAndBody[1] ? msgAndBody[1].trim() : ''; // Ensure body exists
 
             // Example message (PT-3771: Provide option to show user-friendly errors) 
             if (jiraTasksRegex.test(msg)) {
