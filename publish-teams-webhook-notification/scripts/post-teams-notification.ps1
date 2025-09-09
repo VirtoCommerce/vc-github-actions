223537624bd7626e5f7e886babc164d783aacafb
Andrew Kubyshkin <andrew.kubyshkin@virtoway.com>
2025-09-09 16:54:02 +0500

    VCST-2719: Add publish-teams-webhook-notification workflow

---
+++ publish-teams-webhook-notification/scripts/post-teams-notification.ps1
@@ -0,0 +1,51 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$teamsWebhookUrl,
+    [hashtable[]]$mentions,
+    [string]$title,
+    [string]$workflowName,
+    [string]$buildId,
+    [string]$githubOrg,
+    [string]$githubRepo
+)
+$mentionTags = ($mentions | ForEach-Object { "<at>$($_.name)</at>" }) -join ", "
+$body = @{
+    type        = "message"
+    attachments = @(@{
+            contentType = "application/vnd.microsoft.card.adaptive"
+            contentUrl  = $null
+            content     = @{
+                type    = "AdaptiveCard"
+                version = "1.4"
+                body    = @(
+                    @{
+                        type   = "TextBlock"
+                        text   = "🚨 $title"
+                        weight = "Bolder"
+                        color  = "Attention"
+                        size   = "Large"
+                    },
+                    @{
+                        type = "TextBlock"
+                        text = "$mentionTags please check pipeline **$workflowName** - Run **$buildId** - [View Run](https://github.com/$githubOrg/$githubRepo/actions/runs/$buildId)"
+                        wrap = $true
+                    }
+                )
+                msteams = @{
+                    entities = @(
+                        $mentions | ForEach-Object {
+                            @{
+                                type      = "mention"
+                                text      = "<at>$($_.name)</at>"
+                                mentioned = @{
+                                    id   = $_.id
+                                    name = $_.name
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        })
+} | ConvertTo-Json -Depth 10
+Invoke-RestMethod -Uri $teamsWebhookUrl -Method Post -Body $body -ContentType 'application/json'
