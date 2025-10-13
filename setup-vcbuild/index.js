0d4c77a6274228dda302911b84af1e6dca37daec
krankenbro <42555001+krankenbro@users.noreply.github.com>
2025-10-13 09:58:29 +0200

    Update index.js

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.817.0-alpha.268");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.817.0-alpha.269");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
