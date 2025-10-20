90082c7bf3b80bbdde7340946a994656f059c2e3
krankenbro <42555001+krankenbro@users.noreply.github.com>
2025-10-20 09:35:47 +0200

    Update VirtoCommerce.GlobalTool version to 3.817.0

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -3,7 +3,7 @@ const exec = require('@actions/exec');
 
 async function run()
 {
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.817.0-alpha.269");
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.817.0");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
