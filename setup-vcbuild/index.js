cb502ab1d4888cd56dfe0380a04c2d66403990ad
krankenbro <42555001+krankenbro@users.noreply.github.com>
2026-06-24 15:12:32 +0200

    rollback

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -4,10 +4,7 @@ const exec = require('@actions/exec');
 async function run()
 {
     await exec.exec("dotnet --list-sdks");
-    await exec.exec('dotnet',
-        ['tool', 'install', '--global', 'VirtoCommerce.GlobalTool', '--version', '3.1000.0',
-         '--add-source', 'https://api.nuget.org/v3/index.json', '--ignore-failed-sources'],
-        { cwd });
+    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.1000.0");
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
