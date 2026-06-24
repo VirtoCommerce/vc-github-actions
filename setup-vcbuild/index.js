63a12b9653a80839acf051002ae55e125997593e
krankenbro <42555001+krankenbro@users.noreply.github.com>
2026-06-24 15:11:18 +0200

    Improve vc-build installation

--- setup-vcbuild/index.js
+++ setup-vcbuild/index.js
@@ -4,7 +4,10 @@ const exec = require('@actions/exec');
 async function run()
 {
     await exec.exec("dotnet --list-sdks");
-    await exec.exec("dotnet tool install --global VirtoCommerce.GlobalTool --version 3.1000.0");
+    await exec.exec('dotnet',
+        ['tool', 'install', '--global', 'VirtoCommerce.GlobalTool', '--version', '3.1000.0',
+         '--add-source', 'https://api.nuget.org/v3/index.json', '--ignore-failed-sources'],
+        { cwd });
     core.addPath('/home/runner/.dotnet/tools/');
 }
 
